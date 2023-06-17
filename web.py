e7a7d5ef98bb6d2f45aabe0be22d4078e7c304e4
hjee02018@naver.com <hjee02018@naver.com>
2023-06-17 16:21:08 +0900

    [FIX] : 가상환경에 flask 설치

--- web.py
+++ web.py
@@ -1,27 +1,13 @@
 import os
+from flask import Flask, render_template, url_for
 
-# 주어진 HTML 파일을 열어서 내용을 읽어옵니다.
-with open('index.html', 'r') as file:
-    html_content = file.read()
+app = Flask(__name__, static_folder='static')
 
-# pdb 파일이 저장된 디렉토리 경로를 지정합니다.
-pdb_directory = 'pdbs/'
+@app.route('/<pdb_id>')
+def viewer1(pdb_id):
+    pdb_filename = f'{pdb_id.upper()}.pdb'
+    pdb_url = url_for('static', filename='pdbs/' + pdb_filename)
+    return render_template('index.html', data_href=pdb_url)
 
-# pdb 파일 목록을 가져옵니다.
-pdb_files = os.listdir(pdb_directory)
-
-# 각 pdb 파일에 대해 HTML 파일을 생성합니다.
-for pdb_file in pdb_files:
-    # pdb 파일의 경로를 생성합니다.
-    pdb_path = os.path.join(pdb_directory, pdb_file)
-
-    # pdb 파일명을 추출합니다. 예: 9LPR_A.pdb -> 9LPR_A
-    pdb_name = os.path.splitext(pdb_file)[0]
-
-    # HTML 파일 내용을 pdb 파일명으로 대체합니다.
-    modified_html_content = html_content.replace('pdbs/9LPR_A.pdb', pdb_path)
-
-    # pdb 파일명으로 HTML 파일을 저장합니다.
-    output_filename = f'{pdb_name}.html'
-    with open(output_filename, 'w') as file:
-        file.write(modified_html_content)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
